05f1a234d4f770b1fba23364014a67a5993e0f61
_ <_>
2020-05-28 07:55:08 +0100

    Test of concurrent connections to listener. Moves to non-blocking listener.

--- src/listener.rs
+++ src/listener.rs
@@ -4,25 +4,48 @@ use std::io;
 use std::str;
 use std::io::{BufRead, Write, BufReader, BufWriter};
 
-// TODO: Document functions.
-
-// TODO: Add method to stop listening.
+/// Listens for TCP connections at the given address.
+/// 
+/// Expects packets of the form "BLOCKCHAIN 1.0\n", to which it will respond 
+/// "ACK\n". For any other packet, it will respond "ERR\n".
 pub fn listen(address: String) {
-    let listener = TcpListener::bind(address).expect("Failed to bind to address.");
+    let listener = TcpListener::bind(address).expect("Failed to bind listener to address.");
+    listener.set_nonblocking(true).expect("Failed to set listener as non-blocking.");
+
     // The listener has its own thread, and generates a thread for each incoming connection.
     thread::spawn(move || {
-        listener.incoming().for_each(|incoming| {
-            thread::spawn(move || {
-                handle_incoming(incoming);
-            });
-        });
+        for stream in listener.incoming() {
+            match stream {
+                Ok(_) => {
+                    thread::spawn(move || {
+                        handle_incoming(stream);
+                    });
+                },
+                // The listener has not received a new connection yet.
+                Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
+                    // TODO: Consider adding a sleep here.
+                    // TODO: Read from port to kill.
+                    continue;
+                },
+                Err(e) => {
+                    // TODO: Print err message in panic.
+                    println!("{}", e);
+                    panic!(e)
+                }
+            }
+        } 
     });
+}
+
+// TODO: Add method to stop listening.
+pub fn stop_listening() {
+
 }
 
 // TODO: Move away from just adding threads indefinitely.
 // TODO: Store packets before ACKing.
 fn handle_incoming(incoming: Result<TcpStream, io::Error>) {
-    let stream = incoming.expect("Connection failed.");
+    let stream = incoming.expect("Incoming connection failed.");
 
     let buf_reader = BufReader::new(&stream);
     let contents = check_packet(buf_reader);
@@ -33,8 +56,8 @@ fn handle_incoming(incoming: Result<TcpStream, io::Error>) {
 
 fn check_packet<R: BufRead>(mut reader: R) -> Result<(), String> {
     let mut line = String::new();
-    // TODO: Handle packets without any new-lines.
-    reader.read_line(&mut line).expect("Reading failed.");
+    // TODO: Handle packets not terminated by new-lines.
+    reader.read_line(&mut line).expect("Reading from incoming connection failed.");
 
     let tokens = line.split_whitespace().collect::<Vec<&str>>();
 
@@ -45,7 +68,6 @@ fn check_packet<R: BufRead>(mut reader: R) -> Result<(), String> {
 }
 
 fn write_response<W: Write>(writer: &mut W, contents: Result<(), String>) {
-    // TODO: Pass error contents back to user.
     match contents {
         Ok(()) => writer.write(b"ACK\n").expect("Writing failed."),
         Err(_) => writer.write(b"ERR\n").expect("Writing failed.")
@@ -57,17 +79,20 @@ mod tests {
     use std::net::TcpStream;
     use std::io::{BufReader, BufWriter, BufRead, Write};
 
-    fn write_to_listener_and_get_response(port: u16, packet_to_write: &[u8]) -> String {
-        let address = format!("localhost:{}", port);
-        super::listen(address.to_string());
+    fn write_to_listener_and_get_response(address: String, packet_to_write: &[u8]) -> String {
+        let stream = TcpStream::connect(address).expect("Failed to connect to server.");
+        write_to_listener(&stream, packet_to_write);
+        return get_response(&stream);
+    }
 
-        let client = TcpStream::connect(address).expect("Failed to connect to server.");
-        let mut buf_reader = BufReader::new(&client);
-        let mut buf_writer = BufWriter::new(&client);
-
+    fn write_to_listener(stream: &TcpStream, packet_to_write: &[u8]) {
+        let mut buf_writer = BufWriter::new(stream);
         buf_writer.write(packet_to_write).expect("Failed to write packet.");
         buf_writer.flush().expect("Failed to flush buffer.");
+    }
 
+    fn get_response(stream: &TcpStream) -> String {
+        let mut buf_reader = BufReader::new(stream);
         let mut response = String::new();
         buf_reader.read_line(&mut response).expect("Failed to read line.");
         return response;
@@ -75,28 +100,50 @@ mod tests {
 
     #[test]
     fn listen_responds_err_to_invalid_packets() {
+        let address = "localhost:10005";
+        super::listen(address.to_string());
+
         let invalid_packets: Vec<&[u8]> = vec![
             b"\n", // Empty packet.
             b"BLOCKCHAIN\n", // First half of a valid packet.
             b"1.0\n" // Second half of a valid packet.
         ];
 
-        let mut starting_port = 10005;
-
         for invalid_packet in invalid_packets {
-            let response = write_to_listener_and_get_response(starting_port, invalid_packet);
+            let response = write_to_listener_and_get_response(address.to_string(), invalid_packet);
             assert_eq!("ERR\n".to_string(), response);
-            starting_port += 1;
         }
     }
 
     #[test]
     fn listen_responds_ack_to_valid_packets() {
+        let address = "localhost:10005";
+        super::listen(address.to_string());
+
         let valid_packet = b"BLOCKCHAIN 1.0\n";
 
-        let response = write_to_listener_and_get_response(10005, valid_packet);
+        let response = write_to_listener_and_get_response(address.to_string(), valid_packet);
         assert_eq!("ACK\n".to_string(), response);
     }
 
-    // TODO: Test multiple connections.
+    #[test]
+    fn listen_responds_to_multiple_connections_concurrently() {
+        let address = "localhost:10005";
+        super::listen(address.to_string());
+
+        let valid_packet = b"BLOCKCHAIN 1.0\n";
+        let invalid_packet = b"\n";
+
+        // To test concurrent connections, we nest a second write/read inside 
+        // the first.
+        let first_stream = TcpStream::connect(address).expect("Failed to connect to server.");
+        let second_stream = TcpStream::connect(address).expect("Failed to connect to server.");
+        write_to_listener(&first_stream, valid_packet);
+        write_to_listener(&second_stream, invalid_packet);
+        let second_response = get_response(&second_stream);
+        let first_response = get_response(&first_stream);
+
+        assert_eq!("ACK\n".to_string(), first_response);
+        assert_eq!("ERR\n".to_string(), second_response);
+    }
 }
