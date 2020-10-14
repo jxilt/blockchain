ab5859c65b0972451c1e55a2b9aa9996d76d6751
jxilt <_>
2020-10-14 10:28:45 +0100

    Improves error handling for main.rs.

--- src/main.rs
+++ src/main.rs
@@ -9,58 +9,47 @@ mod serverinternal;
 mod handler;
 mod persistence;
 
+const DEFAULT_PORT: &str = "10005";
+
 /// Listens for incoming packets until the user exits the program.
 /// Expects two env arguments: <program name, port>.
 pub fn main() {
     let args = env::args();
-    let port = extract_port_from_args(args);
+    let port = extract_port_from_args(args)
+        .expect("Could not parse port, or flag '-p' provided without corresponding port.");
+    let address = format!("0.0.0.0:{}", port);
 
     let routes = [
         ("/".to_string(), "./src/hello_world.html".to_string())
     ].iter().cloned().collect();
     let mut server = Server::new(routes);
 
-    let address = format!("0.0.0.0:{}", port.expect("TODO: Remove"));
     server.listen(&address).expect("Server could not listen on address.");
-    loop_until_exit_requested(stdin().lock());
+    loop_until_exit_requested(stdin().lock()).expect("Failed to read input.");
     server.stop_listening().expect("Server could not stop listening.");
 }
 
-/// Returns a localhost address based on the port provided using the `-p` flag.
+/// Returns a localhost address based on the port provided using the '-p' flag.
 fn extract_port_from_args(mut args: Args) -> Result<String, String> {
     loop {
         match args.next() {
             None => {
-                let default_port = "10005";
-                println!("No port provided. Using default of '{}'.", default_port);
-                return Ok(default_port.to_string());
+                println!("No port provided. Using default of '{}'.", DEFAULT_PORT);
+                return Ok(DEFAULT_PORT.to_string());
             },
             Some(maybe_port_flag) => {
                 if maybe_port_flag == "-p".to_string() {
                     break;
-                } else {
-                    continue;
                 }
             }
         }
     }
 
     // The port should be the argument following the '-p' flag.
-    let maybe_port = args.next();
-    return match maybe_port {
-        None => Err("Flag \"-p\" used but no port provided.".to_string()),
-        Some(port) => {
-            let maybe_numeric_port = port.parse::<i32>();
-
-            match maybe_numeric_port {
-                Ok(_) => {
-                    println!("Using provided port of {}.", port);
-                    Ok(port)
-                },
-                Err(_) => Err(format!("Flag \"-p\" used but port had incorrect format: {}.", port))
-            }
-        }
-    }
+    let port = args.next().ok_or("Flag '-p' used but no port provided.")?;
+    port.parse::<i32>().map_err(|e| e.to_string())?;
+    println!("Using provided port of {}.", port);
+    return Ok(port);
 }
 
 /// Loop until the reader reads the word 'exit' (plus optional whitespace).
